94aca60528c50b2abd51d70481e7782f4b8ffa2f
hongwongi-sku <hongs0710@skuniv.ac.kr>
2025-08-05 15:37:49 +0900

    1536

---
+++ src/main/java/pack/Spring03Application.java
@@ -0,0 +1,19 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class Spring03Application {
+	
+	@GetMapping("/")
+	public String tiger() {
+		return "Spring03:1536";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring03Application.class, args);
+	}
+
+}
